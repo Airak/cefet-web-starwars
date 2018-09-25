ce5dc727db6cb33793ff8446fcf38e06fcfcd0de
Aluno <matheus.mmag@gmail.com>
2018-09-25 11:48:19 -0300

    Lista de filmes

--- starwars.js
+++ starwars.js
@@ -3,3 +3,28 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+let itemFilme = document.querySelector('#movies ul');
+
+$.ajax({
+  url: 'https://swapi.co/api/films/',
+  dataType: 'json',
+
+  success: function(resposta) {
+  	console.log(resposta);
+   for (movie of resposta.results){
+
+   		let filme = document.createElement('li');
+   		filme.dataset.episodeUrl= movie.url;
+   		filme.innerHTML = "Episode " + movie.episode_id + ": " + movie.title;
+   		itemFilme.appendChild(filme);
+
+
+   		// console.log(movie.title);
+   		// console.log(movie.episode_id);
+   		// console.log(opeaning_crawl);
+   		// console.log(url);
+   }
+  }
+
+});
